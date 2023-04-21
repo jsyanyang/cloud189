0d86e8b65f8fb5001648ca210af28a20417a26c8
jsyang <930256283@qq.com>
2023-04-21 09:57:57 +0000

    debug

--- src/request.ts
+++ src/request.ts
@@ -7,10 +7,10 @@ export const request = axios.create({
     headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/107.0.0.0"
     },
-    proxy: {
-        host: '127.0.0.1',
-        port: '1080'
-    }
+    // proxy: {
+    //     host: '127.0.0.1',
+    //     port: '1080'
+    // }
 });
 
 request.interceptors.response.use(
